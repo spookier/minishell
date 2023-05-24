969ffab44cbefb7d110bc9c32d92d135a6de53e1
spookier <77325667+spookier@users.noreply.github.com>
2023-05-24 18:10:20 +0200

    20230524_18:10

--- incs/minishell.h
+++ incs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: yhwang <yhwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 15:52:50 by yhwang            #+#    #+#             */
-/*   Updated: 2023/05/24 16:29:48 by yhwang           ###   ########.fr       */
+/*   Updated: 2023/05/24 18:03:25 by yhwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,20 @@
 # define STDOUT			1
 # define STDERR			2
 
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <signal.h>
 # include <sys/types.h>
 # include <sys/wait.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <sys/errno.h>
+# include <limits.h>
+# include "../libft/incs/libft.h"
 
 /* main.c */
 void	minishell_header(void);
