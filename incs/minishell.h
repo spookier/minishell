ff77a279dbcc6c23d05acedd386241d6bc719e1c
sleepychloe <hyj2132@gmail.com>
2023-05-24 16:11:19 +0200

    20230524_16:11

--- incs/minishell.h
+++ incs/minishell.h
@@ -6,13 +6,21 @@
 /*   By: yhwang <yhwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 15:52:50 by yhwang            #+#    #+#             */
-/*   Updated: 2023/05/24 15:56:22 by yhwang           ###   ########.fr       */
+/*   Updated: 2023/05/24 16:06:15 by yhwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MIHISHELL_H
 
+# define BLACK			"\x1b[0m"
+# define RED			"\x1b[31m"
+# define CYAN			"\x1b[36m"
+# define YELLOW			"\x1b[33m"
+
+#include <readline/readline.h>
+#include <readline/history.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #endif
