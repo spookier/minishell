283e3fedfcfac8d12e2893f0f1861632897d073c
spookier <77325667+spookier@users.noreply.github.com>
2023-05-24 16:11:19 +0200

    20230524_16:11

--- srcs/main.c
+++ srcs/main.c
@@ -6,14 +6,39 @@
 /*   By: yhwang <yhwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 15:52:37 by yhwang            #+#    #+#             */
-/*   Updated: 2023/05/24 15:54:00 by yhwang           ###   ########.fr       */
+/*   Updated: 2023/05/24 16:07:51 by yhwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../incs/minishell.h"
 
-int	main(void)
+int	minishell_main(char **env)
 {
-	printf("ok/n");
+	char	*rdline;
+	
+	while (1)
+	{
+		//ft_signal();
+		printf("%s", CYAN);
+		rdline = readline("minishell$ ");
+		printf("%s", BLACK);
+		add_history(rdline);
+		printf("%s\n", rdline);
+		free(rdline);
+	}
+	(void)env;
+	return (0);
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	(void)argv;
+	if (argc != 1)
+	{
+		printf("%sArgument error%s\n", RED, BLACK);
+		printf("%sUseage: ./minishell%s\n", RED, BLACK);
+		return (1);
+	}
+	minishell_main(env);
 	return (0);
 }
