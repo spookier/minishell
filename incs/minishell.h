e804e2ce64251f5d737ae8297f1d74b75e694ec2
sleepychloe <hyj2132@gmail.com>
2023-05-24 17:07:09 +0200

    20230524_17:07

--- incs/minishell.h
+++ incs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: yhwang <yhwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 15:52:50 by yhwang            #+#    #+#             */
-/*   Updated: 2023/05/24 16:06:15 by yhwang           ###   ########.fr       */
+/*   Updated: 2023/05/24 16:29:48 by yhwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,27 @@
 # define CYAN			"\x1b[36m"
 # define YELLOW			"\x1b[33m"
 
+# define STDIN			0
+# define STDOUT			1
+# define STDERR			2
+
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+
+/* main.c */
+void	minishell_header(void);
+int	minishell_main(char **env);
+int	main(int argc, char **argv, char **env);
+
+/* signal.c */
+void	signal_handler(int signo);
+void	ft_signal(void);
+
 
 #endif
