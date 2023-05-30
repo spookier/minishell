562e83e8687d44147f434fbbda25097470d4c04c
spookier <77325667+spookier@users.noreply.github.com>
2023-05-30 04:57:32 +0200

    20230530_04:57

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: yhwang <yhwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 02:35:34 by yhwang            #+#    #+#             */
-/*   Updated: 2023/05/29 03:50:25 by yhwang           ###   ########.fr       */
+/*   Updated: 2023/05/30 04:56:56 by yhwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,95 @@ t_data	**alloc_cmd(t_data **cmd, int i)
 	return (cmd);
 }
 
+char	*remove_str_from_line(char *line, int str_start_pos, int str_len)
+{
+	//edit from here
+	int	i;
+
+	i = str_start_pos + str_len - 1;
+	while (line[++i] != '\0')
+		line[str_start_pos] = line[i];
+	line[str_start_pos] = '\0';
+	return (line);
+}
+
+char	*remove_dollar(char *line)
+{
+	int	i;
+	//int	j;
+
+	i = 0;
+	while (line[i] != '\0')
+	{
+		if (line[i] == '$')
+		{	
+			if (line[i + 1] != '\0'
+				&& (line[i + 1] == '\'' || line[i + 1] == '"'))
+			{
+				line = remove_str_from_line(line, i, 1);
+				// j = i;
+				// while (line[++j] != '\0')
+				// 	line[j - 1] = line[j];
+				// line[j - 1] = '\0';
+			}
+		}
+		i++;
+	}
+	return (line);
+}
+
+char	*remove_quote(char *line)
+{
+	char	quote;
+	int		i;
+	int		j;
+	int		pos;
+
+	i = 0;
+	while (line[i] != '\0')
+	{
+		if (line[i] == '\'' || line[i] == '"')
+		{
+			quote = line[i];
+			j = i;
+			while (line[++j] != '\0')
+				line[j - 1] = line[j];
+			line[j - 1] = '\0';
+			pos = find_c_pos(line, quote, i);
+			j = pos;
+			while (line[++j] != '\0')
+				line[j - 1] = line[j];
+			line[j - 1] = '\0';
+			i = pos - 1;
+		}
+		i++;
+	}
+	return (line);
+}
+
+// char	*handle_env_var(char *line)
+// {
+// 	int	i;
+// 	int	j;
+
+// 	i = 0;
+// 	while (line[i] != '\0')
+// 	{
+
+// 	}
+// 	return (line);
+// }
+
 t_data	**parse(t_data **cmd, char **env, char *rdline)
 {
-	//todo: replace with value of env variable
-	//don't free cmd, env
-	
 	char	**split_pipe;
 	int		i;
 
 	if (token_quote_err(rdline) || token_err(rdline) || pos_err(rdline))
 		return (free_cmd(cmd), NULL);
+	rdline = remove_dollar(rdline);
+	rdline = remove_quote(rdline);
+	//rdline = handle_env_var(rdline);
 	printf("%srdline: %s%s\n", CYAN, rdline, BLACK);//
 	split_pipe = ft_split(rdline, '|');
 	i = -1;
