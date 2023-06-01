9457f5ee94479dad2ddbb0f245e13c8c14021bd3
sleepychloe <hyj2132@gmail.com>
2023-06-01 03:55:58 +0200

    20230601_03:55

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: yhwang <yhwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 02:35:34 by yhwang            #+#    #+#             */
-/*   Updated: 2023/05/31 22:46:53 by yhwang           ###   ########.fr       */
+/*   Updated: 2023/06/01 03:55:24 by yhwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ t_data	**alloc_cmd(t_data **cmd, int i)
 	return (cmd);
 }
 
-char	*remove_dollar(char *line)
+void	remove_dollar(char *line)
 {
 	int	i;
 	int	pos;
@@ -47,47 +47,43 @@ char	*remove_dollar(char *line)
 			if (line[i + 1] != '\0'
 				&& (line[i + 1] == '\'' || line[i + 1] == '"'))
 			{
-				line = remove_str_from_line(line, i, 1);
+				line = remove_str_from_line(line, i, ft_strlen("$"));
 				i--;
 			}
 		}
 		i++;
 	}
-	return (line);
 }
 
-char	*remove_quote(char *line)
+char	*change_key_to_value(char *line, char *key, int pos_key_start, char *value)
 {
-	char	quote;
+	char	*line_before_env;
+	char	*line_after_env;
 	int		i;
-	int		pos;
+	int		j;
 
-	i = 0;
-	while (line[i] != '\0')
-	{
-		if (line[i] == '\'' || line[i] == '"')
-		{
-			quote = line[i];
-			line = remove_str_from_line(line, i, 1);
-			pos = find_c_pos(line, quote, i);
-			line = remove_str_from_line(line, pos, 1);
-			i = pos - 1;
-		}
-		i++;
-	}
-	return (line);
-}
-
-char	*change_key_to_value(char *line, char *key, char *value)
-{
-	printf("key: %s\n", key);
-	printf("value: %s\n", value);
+	line_before_env = ft_substr(line, 0, pos_key_start);
+	line_after_env = ft_substr(line, pos_key_start + ft_strlen(key),
+		ft_strlen(line) - ft_strlen(line_before_env));
+	line = ft_realloc(line, ft_strlen(line),
+		ft_strlen(line_before_env) + ft_strlen(line_after_env) + ft_strlen(value) + 1);
+	i = -1;
+	while (line_before_env[++i])
+		line[i] = line_before_env[i];
+	j = -1;
+	while (value[++j])
+		line[i++] = value[j];
+	j = -1;
+	while (line_after_env[++j])
+		line[i++] = line_after_env[j];
 	free(key);
 	free(value);
+	free(line_before_env);
+	free(line_after_env);
 	return (line);
 }
 
-char	*convert_env(char **env, char *line, int start, int end)
+char	*env_check_value(char **env, char *line, int start, int end)
 {
 	char	*key_value[2];
 	char	*temp;
@@ -113,13 +109,29 @@ char	*convert_env(char **env, char *line, int start, int end)
 		key_value[VALUE] = ft_itoa(g_exit_code);
 	if (!key_value[VALUE])
 		key_value[VALUE] = ft_strdup("");
-	return (change_key_to_value(line, key_value[KEY], key_value[VALUE]));
+	return (change_key_to_value(line, key_value[KEY], start, key_value[VALUE]));
+}
+
+char	*env_var_convert_line(char **env, char *line, int i)
+{
+	int	pos;
+
+	remove_str_from_line(line, i, ft_strlen("$"));
+	pos = find_c_pos(line, ' ', i);
+	if (pos == -1)
+		pos = ft_strlen(line);
+	return (env_check_value(env, line, i, pos));
+}
+
+void	handle_env_var_norminette(int *quote)
+{
+	if (*quote == 2)
+		*quote = 0;
 }
 
 char	*handle_env_var(char **env, char *line)
 {
 	int	i;
-	int	pos;
 	int	quote;
 
 	i = 0;
@@ -128,43 +140,67 @@ char	*handle_env_var(char **env, char *line)
 	{
 		if (line[i] == '"')
 			quote++;
-		if (quote == 2)
-			quote = 0;
+		handle_env_var_norminette(&quote);
 		if (line[i] == '\'' && !quote)
 		{
-			i++;
-			pos = find_c_pos(line, line[i - 1], i);
-			i = (pos + 1);
+			i = find_c_pos(line, line[i], i + 1) + 1;
 			continue ;
 		}
 		if (line[i] == '$' && (quote == 0 || quote == 1))
 		{
-			if (!line[i + 1] || (line[i + 1] && (line[i + 1] == '\''
+			if (!(line[i + 1]) || (line[i + 1] && (line[i + 1] == '\''
 						|| line[i + 1] == '"' || line[i + 1] == ' ')))
 				break ;
-			remove_str_from_line(line, i, 1);
-			pos = find_c_pos(line, ' ', i);
-			if (pos == -1)
-				pos = ft_strlen(line);
-			line = convert_env(env, line, i, pos);
+			line = env_var_convert_line(env, line, i);
 		}
 		i++;
 	}
 	return (line);
 }
 
+void	remove_quote(char *line)
+{
+	char	quote;
+	int		i;
+	int		pos;
+
+	i = 0;
+	while (line[i] != '\0')
+	{
+		if (line[i] == '\'' || line[i] == '"')
+		{
+			quote = line[i];
+			line = remove_str_from_line(line, i, ft_strlen("'"));
+			pos = find_c_pos(line, quote, i);
+			line = remove_str_from_line(line, pos, ft_strlen("'"));
+			i = pos - 1;
+		}
+		i++;
+	}
+}
+
+char	*make_new_line(char **env, char *rdline)
+{
+	char	*line;
+
+	line = ft_strdup(rdline);
+	remove_dollar(line);
+	line = handle_env_var(env, line);
+	remove_quote(line);
+	return (line);
+}
+
 t_data	**parse(t_data **cmd, char **env, char *rdline)
 {
+	char	*line;
 	char	**split_pipe;
 	int		i;
 
 	if (token_quote_err(rdline) || token_err(rdline) || pos_err(rdline))
 		return (free_cmd(cmd), NULL);
-	rdline = remove_dollar(rdline);
-	rdline = handle_env_var(env, rdline);
-	rdline = remove_quote(rdline);
-	printf("%srdline: %s%s\n", CYAN, rdline, BLACK);//
-	split_pipe = ft_split(rdline, '|');
+	line = make_new_line(env, rdline);
+	printf("%sline: %s%s\n", CYAN, line, BLACK);//
+	split_pipe = ft_split(line, '|');
 	i = -1;
 	while (split_pipe[++i])
 	{
@@ -173,6 +209,7 @@ t_data	**parse(t_data **cmd, char **env, char *rdline)
 			return (free_2d_arr(split_pipe), NULL);
 		cmd[i]->command = split_pipe[i];
 	}
+	free(line);
 	free_2d_arr(split_pipe);
 	(void)env;
 	return (cmd);
