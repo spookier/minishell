a41737e3fd273947efb4c4dcc90de0452cbcad1e
sleepychloe <hyj2132@gmail.com>
2023-09-18 05:11:35 +0200

    20230918_05:11

--- srcs/execute/execute_builtin_export.c
+++ srcs/execute/execute_builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: yhwang <yhwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 04:57:15 by yhwang            #+#    #+#             */
-/*   Updated: 2023/09/18 04:58:49 by yhwang           ###   ########.fr       */
+/*   Updated: 2023/09/18 05:07:59 by yhwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 
 void	builtin_export(t_data *cmd, char **env)
 {
-	
+	(void)cmd;
+	(void)env;
 }
