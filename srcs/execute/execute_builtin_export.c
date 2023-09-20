cc03a8f8e7e15a188e18cba4a84d8f1231eafc2c
spookier <77325667+spookier@users.noreply.github.com>
2023-09-20 19:29:46 +0200

    starting export

--- srcs/execute/execute_builtin_export.c
+++ srcs/execute/execute_builtin_export.c
@@ -12,8 +12,61 @@
 
 #include "../../incs/minishell.h"
 
+int check_if_env_exists(char *env, char *str)
+{
+	int		i;
+	char	*env_to_find;
+
+	i = 0;
+	while (env[i] && env[i] != '=')
+		i++;
+	env_to_find = malloc((sizeof(char) * i) + 1);
+	if (!env_to_find)
+		return (0);
+	i = 0;
+	while (env[i] && env[i] != '=')
+	{
+		env_to_find[i] = env[i];
+		i++;
+	}
+	env_to_find[i] = 0x00;
+
+	// IF ENV ALREADY EXISTS, RET 1
+	if(ft_strcmp(env_to_find, str) == 0)
+	{
+		free(env_to_find);
+		return(1);
+	}
+	else
+	{
+		free(env_to_find);
+		return(0);
+	}
+}
+
 void	builtin_export(t_data *cmd, char **env)
 {
-	(void)cmd;
-	(void)env;
+	int i;
+	int k;
+
+	k = 0;
+	while (cmd->option && cmd->option[k])
+	{
+		i = 0;
+		while (env[i])
+		{
+			if (check_if_env_exists(env[i], cmd->option[k]) == 1)
+			{
+				// MODIFY EXISTING ENV VALUE HERE
+				printf("Export command already exists\n");
+				break ;
+			}
+			i++;
+		}
+		if(check_if_env_exists(env[i], cmd->option[k]) == 0)
+		{
+			printf("NEW ENV ADDED\n");
+		}
+		k++;
+	}
 }
