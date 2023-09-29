a6b6cdee58ea4775c6a208e831faa9b797730e23
spookier <77325667+spookier@users.noreply.github.com>
2023-09-29 03:38:05 +0200

    20230929_03:38

--- srcs/parse/parse_fill_cmd_struct.c
+++ srcs/parse/parse_fill_cmd_struct.c
@@ -6,11 +6,50 @@
 /*   By: yhwang <yhwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 01:51:02 by yhwang            #+#    #+#             */
-/*   Updated: 2023/09/24 16:40:53 by yhwang           ###   ########.fr       */
+/*   Updated: 2023/09/29 03:26:57 by yhwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incs/minishell.h"
+
+int	is_redir(char *str)
+{
+	if ((!strncmp("<", str, 1) && ft_strlen(str) == 1)
+		|| (!strncmp(">", str, 1) && ft_strlen(str) == 1)
+		|| (!strncmp("<<", str, 1) && ft_strlen(str) == 2)
+		|| (!strncmp(">>", str, 1) && ft_strlen(str) == 2))
+		return (1);
+	return (0);
+}
+
+void	fill_command(t_data **cmd, char **split_cmd, int cmd_i)
+{
+	if (!is_redir(split_cmd[0]))
+		cmd[cmd_i]->command = ft_strdup(split_cmd[0]);
+	else
+		cmd[cmd_i]->command = ft_strdup("");
+}
+
+void	fill_option(t_data **cmd,
+			char *each_cmd, char **split_cmd, int cmd_i)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	cmd[cmd_i]->option[i] = NULL;
+	while (split_cmd[++i])
+	{
+		if (is_redir(split_cmd[i]))
+		{
+			i++;
+			continue ;
+		}
+		cmd[cmd_i]->option[j++] = ft_strdup(split_cmd[i]);
+	}
+	check_redir(cmd, each_cmd, split_cmd, cmd_i);
+}
 
 void	revert_cmd(t_data **cmd)
 {
@@ -40,37 +79,6 @@ void	revert_cmd(t_data **cmd)
 	}
 }
 
-int	is_redir(char *str)
-{
-	if ((!strncmp("<", str, 1) && ft_strlen(str) == 1)
-		|| (!strncmp(">", str, 1) && ft_strlen(str) == 1)
-		|| (!strncmp("<<", str, 1) && ft_strlen(str) == 2)
-		|| (!strncmp(">>", str, 1) && ft_strlen(str) == 2))
-		return (1);
-	return (0);
-}
-
-void	fill_option(t_data **cmd,
-			char *each_cmd, char **split_cmd, int cmd_i)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	cmd[cmd_i]->option[i] = NULL;
-	while (split_cmd[++i])
-	{
-		if (is_redir(split_cmd[i]))
-		{
-			i++;
-			continue ;
-		}
-		cmd[cmd_i]->option[j++] = ft_strdup(split_cmd[i]);
-	}
-	check_redir(cmd, each_cmd, split_cmd, cmd_i);
-}
-
 t_data	**fill_cmd_struct(t_data **cmd, char *each_cmd, int cmd_i)
 {
 	char	**split_each_cmd;
@@ -85,7 +93,7 @@ t_data	**fill_cmd_struct(t_data **cmd, char *each_cmd, int cmd_i)
 		free_2d_arr(split_each_cmd);
 		return (free_cmd(cmd), NULL);
 	}
-	cmd[cmd_i]->command = ft_strdup(split_each_cmd[0]);
+	fill_command(cmd, split_each_cmd, cmd_i);
 	cmd[cmd_i]->option = ft_calloc(sizeof(char *), option);
 	cmd[cmd_i]->redir = ft_calloc(sizeof(t_redir **), 2);
 	cmd[cmd_i]->redir[0] = ft_calloc(sizeof(t_redir *), 2);
